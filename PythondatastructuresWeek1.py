b4ddc3d1d0fc2ac41e00e6877bd3a106c0ddf63e
Rishith Kyatham <rkyatham01@gmail.com>
2022-02-06 16:15:00 -0500

    update

--- PythondatastructuresWeek1.py
+++ PythondatastructuresWeek1.py
@@ -2,15 +2,25 @@
 
 class PartyAnimal: #A template
   x = 0
+  name = ""
+  def __init__(self,z): #constructor, self passes in instance of this particular one
+      self.name = z
+      print(self.name, "constructed")
 
   def part(self):
-      self.x = self.x + 1
-      print("One",self.x)
+    self.x = self.x + 1
+    print(self.name, "party count", self.x)
 
-an = PartyAnimal() #class is a template for an
-an.part()
-an.part() #only takes self so (an), can't give it arguements
-an.part()
-print(type(PartyAnimal))
-print(dir(PartyAnimal))
+class FootballFan(PartyAnimal): #Class gets passed into FootballFan (Inheritance) Extension
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.part()
+        print(self.name,"points",self.points)
 
+x = PartyAnimal("Bob") #a object x is created using template
+x.part()
+print("\r")
+y = FootballFan("Brad") #inherits all the properties from PartyAnimal template + FootballFan due to class being passed in
+y.part()
+y.touchdown()
